d81b2528f6a257c7567b25f9288b5e97f8766bf7
flicker <flickerflak@gmail.com>
2023-08-02 11:47:39 +0530

    1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify
 import dotenv
 import os
 
@@ -8,7 +8,12 @@ dotenv.load_dotenv()
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return  render_template("index.html")
+
+@app.route('/ping')
+def ping():
+
+    return jsonify("pong")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=int(os.getenv('PORT')))
